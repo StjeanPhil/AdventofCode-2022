6796060d0aaa60149bd7e5d4733fe212b9d7153a
StjeanPhil <philippe_spellerin@hotmail.ca>
2022-12-11 13:55:12 -0500

    day10 solved

--- day10/day10.py
+++ day10/day10.py
@@ -18,7 +18,6 @@ def getcycles(data):
             cycles.append(val)
             command, amount = command.split()
             val += int(amount)
-
     return cycles
 
 
@@ -27,7 +26,29 @@ def prob1(cycles):
         cycles[139]*140 + cycles[179]*180 + cycles[219]*220
 
 
+def getMsg(cycles):
+    msg = [[]]  # list of the six lines of pixels
+    for idx in range(240):
+        row = int(idx/40)
+        if row == len(msg):
+            msg.append([])
+        pixelPos = idx % 40
+        spritePos = cycles[idx]
+        if spritePos-1 <= pixelPos <= spritePos+1:
+            msg[row].append("#")
+        else:
+            msg[row].append(".")
+    printVersion = ""
+    for line in msg:
+        for pix in line:
+            printVersion += pix
+        printVersion += "\n"
+    return printVersion
+
+
 if __name__ == "__main__":
 
     cycles = getcycles(file)
-    print(prob1(cycles))
+    # Problem 1:
+    print("Problem 1: ", prob1(cycles))
+    print("Problem 2: \n", getMsg(cycles))
